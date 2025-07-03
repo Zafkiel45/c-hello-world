e0b884a81fc7fdb9a9efae43f2080626611f5245
zafkiel45 <zafkielc@gmail.com>
2025-07-03 17:06:53 -0300

    add some comments

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    // the \n is necessary for later prints at the same line!  
+    // \a does a sound!
+    // \f break lines preserving ident
+    // \b is the backspace
+    printf("Hello, World!\b");
+    printf("I'm really happy!");
     return 0;
 }
